8a8c88e24c3469b48069534fce07d021f031539c
asdf-qwe <fism@naver.com>
2025-05-13 09:34:16 +0900

    WIP

--- be/src/main/java/com/example/mingle/domain/post/legalpost/enums/ContractStatus.java
+++ be/src/main/java/com/example/mingle/domain/post/legalpost/enums/ContractStatus.java
@@ -1,5 +1,5 @@
 package com.example.mingle.domain.post.legalpost.enums;
 
 public enum ContractStatus {
-    DRAFT, REVIEW, SIGNED_OFFLINE, CONFIRMED, ACTIVE, EXPIRED, PENDING,TERMINATED
+    DRAFT, REVIEW, SIGNED_OFFLINE, SIGNED, CONFIRMED, ACTIVE, EXPIRED, PENDING,TERMINATED
 }
