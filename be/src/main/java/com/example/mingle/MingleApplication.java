aed4f4435d8d517c65278a24d86bc6291fe60bd9
asdf-qwe <fism@naver.com>
2025-05-14 15:43:31 +0900

    불필요한 임포트 삭제

--- be/src/main/java/com/example/mingle/MingleApplication.java
+++ be/src/main/java/com/example/mingle/MingleApplication.java
@@ -2,9 +2,6 @@ package com.example.mingle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 public class MingleApplication {
