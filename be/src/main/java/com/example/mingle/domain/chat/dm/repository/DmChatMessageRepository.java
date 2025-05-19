8189a52b747fbae6b6c6ef169fe1020a9e33cf97
Eunseo Lee <monimoni@ewhain.net>
2025-05-19 15:37:43 +0900

    [BE/FEAT] repository - 태그 추가

--- be/src/main/java/com/example/mingle/domain/chat/dm/repository/DmChatMessageRepository.java
+++ be/src/main/java/com/example/mingle/domain/chat/dm/repository/DmChatMessageRepository.java
@@ -1,4 +1,7 @@
 package com.example.mingle.domain.chat.dm.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface DmChatMessageRepository {
 }
--- be/src/main/java/com/example/mingle/domain/chat/group/repository/GroupChatRoomRepository.java
+++ be/src/main/java/com/example/mingle/domain/chat/group/repository/GroupChatRoomRepository.java
@@ -1,4 +1,7 @@
 package com.example.mingle.domain.chat.group.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface GroupChatRoomRepository {
 }
