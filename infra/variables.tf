a8679f12daf6cdb96f0b41978c962e99a84d454e
user <si159521@gmail.com>
2025-05-26 13:14:19 +0900

    tf설정파일, application-prod.yml파일, .gitinore

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "dev-2"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "team_IDLE"
+}
