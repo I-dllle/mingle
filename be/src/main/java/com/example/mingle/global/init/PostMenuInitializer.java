b96dd11d0f7719a617baf2531008231a43576b4e
Eunseo Lee <monimoni@ewhain.net>
2025-05-27 16:43:00 +0900

    [BE/FIX] Initializer - 경영진 부서 삭제

--- be/src/main/java/com/example/mingle/global/init/PostMenuInitializer.java
+++ be/src/main/java/com/example/mingle/global/init/PostMenuInitializer.java
@@ -127,21 +127,22 @@ public class PostMenuInitializer implements ApplicationRunner {
             initializeMenu("SNS_CONTENT", "SNS컨텐츠", "마케팅 - SNS 콘텐츠 게시판", "Marketing & PR", creator);
             initializeMenu("EVENTS", "이벤트", "마케팅 - 이벤트 게시판", "Marketing & PR", creator);
 
-            //경영진/법무팀
-            initializeMenu("NOTICEBOARD_MANAGEMENT", "공지사항 관리", "경영진 - 공지사항 관리", "Finance & Legal", creator);
-            initializeMenu("USER_MANAGEMENT", "사용자 관리", "경영진 - 사용자 계정 관리", "Finance & Legal", creator);
-            initializeMenu("ATTENDANCE_MANAGEMENT", "근태관리", "경영진 - 근태 관리", "Finance & Legal", creator);
-            initializeMenu("REVENUE_ANALYSIS", "수익분석", "경영진 - 수익분석 기능", "Finance & Legal", creator);
-            initializeMenu("SETTLEMENT_MANAGEMENT", "정산관리", "경영진 - 정산 내역 관리", "Finance & Legal", creator);
-            initializeMenu("CONTRACT_MANAGEMENT", "계약서관리", "경영진 - 계약 문서 관리", "Finance & Legal", creator);
+            //경영진(추후구현)
+
 
             //정산/법무팀
             initializeMenu("REVENUE_SETTLEMENT", "수익 정산 내역", "수익 정산 내역 등록 게시판", "Finance & Legal", creator);
             initializeMenu("CONTRACT_MANAGEMENT", "계약서관리", "계약서 업로드", "Finance & Legal", creator);
             initializeMenu("CONTRACT_RENEWALS", "갱신 관리", "계약서갱신 관리", "Finance & Legal", creator);
 
-            //경영진/관리자
+            //운영진
             initializeMenu("DASHBOARD", "대시보드", "대시보드 시각화 요약", "System Operations", creator);
+            initializeMenu("NOTICEBOARD_MANAGEMENT", "공지사항 관리", "운영팀 - 공지사항 관리", "System Operations", creator);
+            initializeMenu("USER_MANAGEMENT", "사용자 관리", "운영팀 - 사용자 계정 관리", "System Operations", creator);
+            initializeMenu("ATTENDANCE_MANAGEMENT", "근태관리", "운영팀 - 근태 관리", "System Operations", creator);
+            initializeMenu("REVENUE_ANALYSIS", "수익분석", "운영팀 - 수익분석 기능", "System Operations", creator);
+            initializeMenu("SETTLEMENT_MANAGEMENT", "정산관리", "운영팀 - 정산 내역 관리", "System Operations", creator);
+            initializeMenu("CONTRACT_MANAGEMENT", "계약서관리", "운영팀 - 계약 문서 관리", "System Operations", creator);
 
             //아티스트/매니저
             initializeMenu("ARTIST_REPORT", "활동보고서", "매니저/아티스트 - 활동보고서 게시판", "Artist & Manager", creator);
