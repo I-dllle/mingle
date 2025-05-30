e9cb108a6dc8221ae0449bfe74b6e5e30764c238
Eunseo Lee <monimoni@ewhain.net>
2025-05-30 13:31:45 +0900

    Revert "Merge branch 'dev' of https://github.com/I-dllle/mingle into feature/fe/143"
    This reverts commit a631d99ef6ac574d5ba5f5b80964d59b04d8725b, reversing
    changes made to 566d4195620f1e9afbf56adf0722329e272b20d0.

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -1,9 +1,9 @@
-import "./globals.css";
-import { AuthProvider } from "@/features/auth/AuthProvider";
+import './globals.css';
+import { AuthProvider } from '@/features/auth/AuthProvider';
 
 export const metadata = {
-  title: "Mingle",
-  description: "엔터테인먼트 그룹 통합 솔루션",
+  title: 'Mingle',
+  description: '엔터테인먼트 그룹 통합 솔루션',
 };
 
 export default function RootLayout({
