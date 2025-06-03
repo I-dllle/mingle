825cccd1ed0b24e3d5a1fec5156a7e73aaf28e03
Si159521 <101044294+Si159521@users.noreply.github.com>
2025-06-04 01:23:58 +0900

    Update LoginForm.tsx

--- fe/src/features/user/auth/components/LoginForm.tsx
+++ fe/src/features/user/auth/components/LoginForm.tsx
@@ -14,7 +14,7 @@ export default function LoginForm() {
     e.preventDefault();
 
     try {      // 로그인 요청
-      await apiClient('/users/login', {
+      await apiClient('/api/v1/users/login', {
         method: 'POST',
         body: JSON.stringify({ loginId, password }),
         credentials: 'include',
